c6f258c6a17346c4741b78df30a2d06460ea13d0
Richard Chibuike <franklinrichard2007@gmail.com>
2026-07-17 16:08:25 +0100

    refactor: register webhook dependencies into invoices context module

--- src/invoices/invoices.module.ts
+++ src/invoices/invoices.module.ts
@@ -3,9 +3,10 @@ import { InvoicesController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { SorobanModule } from '../common/soroban/soroban.module';
+import { WebhooksModule } from '../webhooks/webhooks.module';   // ← new
 
 @Module({
-  imports: [PrismaModule, SorobanModule],
+  imports: [PrismaModule, SorobanModule, WebhooksModule],   // ← added
   controllers: [InvoicesController],
   providers: [InvoicesService],
   exports: [InvoicesService],
